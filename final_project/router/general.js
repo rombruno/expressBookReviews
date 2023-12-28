7c78de8ee8ae8da766c18df19784573e3804a447
rombruno <rom_bruno@hotmail.com>
2023-12-28 14:19:54 -0500

    update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,14 @@ public_users.get("/isbn/:isbn",function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const books_keys = Object.keys(books);
+    books_keys.forEach((key) => { 
+        let book = books[key];
+        if(book["author"] === author) {
+            res.send(JSON.stringify(book,null,4)); 
+        }
+    })
 });
 
 // Get all books based on title
