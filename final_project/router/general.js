44522f199f681a0803cbbc27672be5fb9be82b37
rombruno <rom_bruno@hotmail.com>
2024-01-08 07:06:30 -0500

    update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,24 +31,10 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
-//const promise1 = new Promise((myresolve,myreject)=>{
-//        public_users.get('/',function (req, res) {
-//            try {
-//                        res.send(JSON.stringify(books,null,4));
-//                        myresolve("response successfully sent for GET /");
-//            } catch(err) {
-//                myreject("response not sent because of an error")
-//                return res.status(404).json({message: "Unable to handle request"});
-//            }
-//        });      
-//});
-
-
 public_users.get('/',function (req, res) {
-    const promise1 = new Promise((myresolve,myreject)=>{
+    const promise = new Promise((myresolve,myreject)=>{
         try {
-            setTimeout(() => {
+            setTimeout(() => {      //setTimeout function allows to run asynchronously
                     res.send(JSON.stringify(books,null,4));
                     myresolve("response successfully sent for GET /");
             },10000)    
@@ -57,63 +43,98 @@ public_users.get('/',function (req, res) {
             return res.status(404).json({message: "Unable to handle request"});
         }  
     });
-    promise1.then(
+    promise.then(
         (successMessage) => console.log(successMessage),
         (errorMessage) => console.log(errorMessage) 
-    );
-    
+    );  
 }); 
-
-//promise1.then(
-//    (successMessage) => console.log(successMessage),
-//    (errorMessage) => console.log(errorMessage) 
-//  );
-
 
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn",function (req, res) {
-    const isbn = req.params.isbn;
-    let book = books[isbn];
-    if(book) {
-        res.send(JSON.stringify(book,null,4)); 
-    } else {
-        res.send("Unable to find book!");
-    }
+    const promise = new Promise((myresolve,myreject)=>{
+        try {
+            setTimeout(() => {      //setTimeout function allows to run asynchronously
+                const isbn = req.params.isbn;
+                let book = books[isbn];
+                if(book) {
+                    res.send(JSON.stringify(book,null,4));
+                    myresolve("response successfully sent for GET /isbn/:isbn");
+                } else {
+                    res.send("Unable to find book!");
+                }
+            },10000)
+        } catch(err) {
+            myreject("response not sent because of an error")
+            return res.status(404).json({message: "Unable to handle request"});
+        }
+    });
+    promise.then(
+        (successMessage) => console.log(successMessage),
+        (errorMessage) => console.log(errorMessage) 
+    );
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    const books_keys = Object.keys(books);
-    let matched_book = "";
-    books_keys.forEach((key) => {
-        let book = books[key];
-        if(book["author"] === author) {
-            matched_book += '\n' + JSON.stringify(book,null,4);
+    const promise = new Promise((myresolve,myreject)=>{
+        try {
+            setTimeout(() => {      //setTimeout function allows to run asynchronously
+                const author = req.params.author;
+                const books_keys = Object.keys(books);
+                let matched_book = "";
+                books_keys.forEach((key) => {
+                    let book = books[key];
+                    if(book["author"] === author) {
+                        matched_book += '\n' + JSON.stringify(book,null,4);
+                    }
+                })
+                if(matched_book === "") {
+                matched_book = "Unable to find author " + author; 
+                }
+                res.send(matched_book);
+                myresolve("response successfully sent for GET /author/:author");
+            },10000)
+        } catch(err) {
+            myreject("response not sent because of an error")
+            return res.status(404).json({message: "Unable to handle request"});
         }
-    })
-    if(matched_book === "") {
-       matched_book = "Unable to find author " + author; 
-    }
-    res.send(matched_book);
+    });
+    promise.then(
+        (successMessage) => console.log(successMessage),
+        (errorMessage) => console.log(errorMessage) 
+    );
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title.toLowerCase();
-    const books_keys = Object.keys(books);
-    let matched_book = "";
-    books_keys.forEach((key) => {
-        let book = books[key];
-        if(book["title"].toLowerCase().indexOf(title) >= 0) {
-            matched_book += '\n' + JSON.stringify(book,null,4);
+    const promise = new Promise((myresolve,myreject)=>{
+        try {
+            setTimeout(() => {      //setTimeout function allows to run asynchronously
+                const title = req.params.title.toLowerCase();
+                const books_keys = Object.keys(books);
+                let matched_book = "";
+                books_keys.forEach((key) => {
+                    let book = books[key];
+                    if(book["title"].toLowerCase().indexOf(title) >= 0) {
+                        matched_book += '\n' + JSON.stringify(book,null,4);
+                    }
+                })
+                if(matched_book === "") {
+                    matched_book = "Unable to find title " + title; 
+                }
+                res.send(matched_book);
+                myresolve("response successfully sent for GET /title/:title");
+            },10000)
+        } catch(err) {
+            myreject("response not sent because of an error")
+            return res.status(404).json({message: "Unable to handle request"});
         }
-    })
-    if(matched_book === "") {
-        matched_book = "Unable to find title " + title; 
-     }
-    res.send(matched_book); 
+    });
+    promise.then(
+        (successMessage) => console.log(successMessage),
+        (errorMessage) => console.log(errorMessage) 
+    );
 });
 
 //  Get book review
